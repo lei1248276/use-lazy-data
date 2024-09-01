b6dfe6ca47c9583b95f59fd792f7807bf955622b
Jaye <jaye1248276@gmail.com>
2024-09-01 16:38:24 +0800

    feat: 新增object getter模式

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,56 @@
 /**
- * @description Load data when in use
+ * @description Load when used and only once - (使用时加载且只加载一次)
+ * @param getter () => ({}) | { value: () => ({}) }
+ * @example
  * ```ts
- * const data = useLazyData(fn)
- * console.log(data.value)
+ * import useLazyData from 'use-lazy-data'
+ * const data = useLazyData(() => ({
+ *  name: 'Jaye'
+ * }))
+ * console.log(data) -> "{}"
+ * data.value
+ * console.log(data) -> "{ value: { name: 'Jaye' } }"
+ *
+ * const data2 = useLazyData({
+ *   name: () => 'Jaye',
+ * })
+ * console.log(data2) -> "{}"
+ * data2.name
+ * console.log(data2) -> "{ name: 'Jaye' }"
  * ```
  */
-export default function useLazyData<T extends() => ReturnType<T>>(fn: T): { value: ReturnType<T> } {
-  return {
-    get value() {
-      // @ts-ignore
-      delete this.value
-      // @ts-ignore
-      return (this.value = fn())
+export default function useLazyData<T extends Record<string, () => any> | (() => any)>(
+  getter: T
+): T extends Record<string, () => any>
+  ? { [K in keyof T]: ReturnType<T[K]> }
+  : T extends () => any
+    ? { value: ReturnType<T> }
+    : never {
+  if (typeof getter === 'function') {
+    // @ts-ignore
+    return {
+      get value() {
+        delete this.value
+        return (this.value = getter())
+      }
     }
   }
+
+  if (Object.prototype.toString.call(getter) === '[object Object]') {
+    const obj = {}
+    for (const key in getter) {
+      Object.defineProperty(obj, key, {
+        get() {
+          delete this[key]
+          return (this[key] = (getter[key] as () => any)())
+        },
+        configurable: true,
+        enumerable: true
+      })
+    }
+    // @ts-ignore
+    return obj
+  }
+
+  throw new Error('Invalid getter')
 }
